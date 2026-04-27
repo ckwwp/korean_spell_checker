6db976093a8eb6d8e1af5e88dbf26bf3ab322929
Ink-14 <178154117+Ink-14@users.noreply.github.com>
2026-04-27 21:30:58 +0900

    add: get compatible batchim character function

--- utils/hangul.py
+++ utils/hangul.py
@@ -10,6 +10,7 @@ CHOSEONG_IDX_POINT = JUNGSEONG_IDX_POINT * JONGSEONG_IDX_POINT
 CHO_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
 JUNG_LIST = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
 JONG_LIST = ['', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+BATCHIM_LIST = ['', 'ᆨ', 'ᆩ', 'ᆪ', 'ᆫ', 'ᆬ', 'ᆭ', 'ᆮ', 'ᆯ', 'ᆰ', 'ᆱ', 'ᆲ', 'ᆳ', 'ᆴ', 'ᆵ', 'ᆶ', 'ᆷ', 'ᆸ', 'ᆹ', 'ᆺ', 'ᆻ', 'ᆼ', 'ᆽ', 'ᆾ', 'ᆿ', 'ᇀ', 'ᇁ', 'ᇂ']
 
 CHO_SET = {i for i in CHO_LIST}
 JUNG_SET = {i for i in JUNG_LIST}
@@ -48,6 +49,9 @@ def get_jungseong(char: str) -> str:
 
 def get_jongseong(char: str) -> str:
     return JONG_LIST[(convert_to_hangul_charpoint(char) % JONGSEONG_IDX_POINT)]
+
+def get_compatible_batchim(char) -> str:
+    return BATCHIM_LIST[(convert_to_hangul_charpoint(char) % JONGSEONG_IDX_POINT)]
 
 def remove_batchim(char: str) -> str:
     if len(char) != 1:
